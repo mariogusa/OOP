0748f62a507adfbd2e310ed94b14674563f98a84
mariogusa <gusamario272@yahoo.com>
2024-03-01 12:50:17 +0200

    tema lab1

--- lab1/laborator1-oop.cpp
+++ lab1/laborator1-oop.cpp
@@ -1,3 +1,4 @@
+///problema #1
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -30,5 +31,34 @@ int main()
 
 
     fclose(file);
+    return 0;
+}
+///problema #2
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool compare(const void *a, const void *b) {
+    const char *str1=*(const char **)a;
+    const char *str2=*(const char **)b;
+    int lg1=strlen(str1);
+    int lg2=strlen(str2);
+    if (lg1==lg2)
+        return strcmp(str1,str2)>0;
+    return lg1 < lg2;
+int main() 
+{
+    char s[100];
+    std::cin.getline(s,100);
+    char *cuv[100];
+    char *p=strtok(s," ");
+    int nr=0;
+    while (p!=NULL)
+    {
+        cuv[nr++]=p;
+        p=strtok(NULL," ");
+    }
+    qsort(cuv,nr,sizeof(char *), compare);
+    for(int i=nr-1;i>=0;i--)
+        std::cout<<cuv[i]<<endl;
     return 0;
 }
